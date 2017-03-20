bc4627b0f1294b886d2aec5053bfb8a813fcd8e5
cndaniel <cndaniel@me.com>
2017-03-20 17:07:55 +0100

    basic rack server'

--- config.ru
+++ config.ru
@@ -3,4 +3,4 @@ require File.expand_path '../main.rb', __FILE__
 run Rack::URLMap.new({
   '/' => Public,
   '/api' => Api
-  })
+})
--- main.rb
+++ main.rb
@@ -2,34 +2,31 @@ require 'json'
 require 'jwt'
 require 'sinatra/base'
 
-
 class Api < Sinatra::Base
   def initialize
     super
 
-  @accounts ={
-    tomodelonge: 10000,
-    markhoppus: 50000,
-    travisbarker: 1000000000
-  }
+    @accounts = {
+      tomodelonge: 10_000,
+      markhoppus: 50_000,
+      travisbarker: 1_000_000_000
+    }
   end
-
 
   get '/money' do
     content_type :json
-    { message: "Hello World!"}.to_json
+    { message: 'Hello World!' }.to_json
   end
 end
-
 
 class Public < Sinatra::Base
   def initialize
     super
-  @logins = {
-    tomodelonge: 'allthesmallthings',
-    markhoppus: 'therockshow',
-    travisbarker: 'whatsmyageagain'
-  }
+    @logins = {
+      tomodelonge: 'allthesmallthings',
+      markhoppus: 'therockshow',
+      travisbarker: 'whatsmyageagain'
+    }
   end
   post '/login' do
     username = params[:username]
@@ -37,9 +34,10 @@ class Public < Sinatra::Base
 
     if @logins[username.to_sym] == password
       content_type :json
-      { message: "you logged in. Yay you!" }.to_json
+      { message: 'you logged in. Yay you!' }.to_json
     else
       halt 401
     end
   end
+
 end
